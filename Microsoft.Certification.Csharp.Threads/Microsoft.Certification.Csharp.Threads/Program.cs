1008d9ec6a9141239b3e59303b102efa028b092b
Pierpaolo Paris <dottor.p@gmail.com>
2018-10-30 23:27:13 +0100

    First commit

---
+++ Microsoft.Certification.Csharp.Threads/Microsoft.Certification.Csharp.Threads/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Microsoft.Certification.Csharp.Threads
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
